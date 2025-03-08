be237f61ae425dc2b2db9ee18fc3d6a73c18869a
PhatBee <vinhphatst1235@gmail.com>
2025-03-08 23:32:06 +0700

    Tìm hiểu về SQLite v2: Tạo database, tạo bảng và nhập dữ liệu

--- app/src/main/java/vn/phatbee/tuan6_sqllite/MainActivity.java
+++ app/src/main/java/vn/phatbee/tuan6_sqllite/MainActivity.java
@@ -1,6 +1,8 @@
 package vn.phatbee.tuan6_sqllite;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import vn.phatbee.tuan6_sqllite.Database.DatabaseHandler;
+
 public class MainActivity extends AppCompatActivity {
+    // Khai báo biến toàn cục
+    DatabaseHandler databaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Gọi hàm Database
+
+        // Tạo Database
+    }
+
+    private void createDatabaseSQLite() {
+        // Thêm dữ liệu vào bảng
+        databaseHandler.queryData("INSERT INTO Notes VALUES (NULL, 'Ví dụ về SQLite 1')");
+        databaseHandler.queryData("INSERT INTO Notes VALUES (NULL, 'Ví dụ về SQLite 2')");
+        databaseHandler.queryData("INSERT INTO Notes VALUES (NULL, 'Ví dụ về SQLite 3')");
+    }
+
+    private void initDatabaseSQLite(){
+        // Khởi tạo Database
+        databaseHandler = new DatabaseHandler(this, "notes.SQLite", null, 1);
+
+        // Tạo bảng Notes
+        databaseHandler.queryData("CREATE TABLE IF NOT EXISTS Notes (Id INTEGER PRIMARY KEY AUTOINCREMENT, NameNotes VARCHAR(200))");
+    }
+
+    private void databaseSQLite(){
+        // Lấy dữ liệu
+        Cursor cursor = databaseHandler.getData("SELECT * FROM Notes");
+        while (cursor.moveToNext()){
+            String name = cursor.getString(1);
+            Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        }
     }
 }
